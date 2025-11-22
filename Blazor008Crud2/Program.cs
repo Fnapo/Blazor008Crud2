806b913a9eaf891f8ef05de869ed637a5596c848
Francisco Sola Porcuna <fran_sopo@hotmail.com>
2025-11-22 11:11:04 +0100

    Agregar archivos de proyecto.

---
+++ Blazor008Crud2/Program.cs
@@ -0,0 +1,45 @@
+﻿using B007Clases.Data;
+using Blazor008Crud2.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor008Crud2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configuracion = builder.Configuration;
+            string cadenaConexion = configuracion.GetConnectionString("ConexionDefecto") ?? throw new InvalidOperationException("La cadena de conexi�n no se encontr�.");
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddDbContext<B007CrudContext>(opciones => opciones.UseSqlServer(cadenaConexion));
+
+            builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+    app.UseMigrationsEndPoint();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
